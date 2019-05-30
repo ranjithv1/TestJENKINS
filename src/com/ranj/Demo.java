7e0cf8e89831b85eddaa72de9f2e94bbad18e867
ranjithv1 <30407424+ranjithv1@users.noreply.github.com>
2019-05-30 17:32:52 +0530

    Updated
    Abhijitu

--- src/com/ranj/Demo.java
+++ src/com/ranj/Demo.java
@@ -3,5 +3,6 @@ package com.ranj;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hi I am Ranjith V from India, Bengaluru");
+		System.out.println("Hi I am Ranjith V from India, WB");
 	}
 }
